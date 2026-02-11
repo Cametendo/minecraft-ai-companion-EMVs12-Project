aaec5f51280ed6f6166c935e89aa83811b65cc44
Adam237A <hussadi345@gmail.com>
2026-02-11 08:24:50 +0100

    AI Mod (nicht fertig)

---
+++ AiCompanion1.0/src/client/java/ollamacompanion/client/OllamacompanionClient.java
@@ -0,0 +1,10 @@
+package ollamacompanion.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OllamacompanionClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
